64971dd9ff79b9fc968bf91092204b55ffdac68e
Saccullo-p <saccullo.pietro@iisgalvanimi.edu.it>
2023-01-27 23:04:19 +0000

    pronto

--- flask/app.py
+++ flask/app.py
@@ -6,7 +6,7 @@ from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 
-app.config["MONGO_URI"] = "mongodb://sac:Camera1@cluster0-shard-00-00.x1eis.mongodb.net:27017,cluster0-shard-00-01.x1eis.mongodb.net:27017,cluster0-shard-00-02.x1eis.mongodb.net:27017/Progetto_Sito?ssl=true&replicaSet=atlas-20bimn-shard-0&authSource=admin&retryWrites=true&w=majority"
+app.config["MONGO_URI"] = "" # Stringa di connessione a MongoDB
 
 mongo = PyMongo(app)
 # Per rispondere alle chiamate cross origin
